f4aa3fba79d9a6d6f7e73cf492bf6835999daa11
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2019-01-30 15:42:33 +0000

    some of the changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import styled from 'styled-components/macro'
-import { fromEvent, Observable } from 'rxjs';
+import { fromEvent, Observable, interval, Subscriber, Observer, Subscription } from 'rxjs';
 import { map, bufferCount } from 'rxjs/operators'
+import { string } from 'prop-types';
 
 const Container = styled.div`
     background-color: white;
@@ -12,8 +13,27 @@ const Container = styled.div`
 interface State {
     total: number
 }
+
+const x = new Observable<number>((observer) => {
+    let ticks: number = 0;
+    const interval = setInterval(() => {
+        observer.next(ticks)
+        ticks++
+        if (ticks === 11) {
+            observer.complete()
+        }
+
+    }, 500)
+
+    return () => {
+        console.log('disposed')
+        clearInterval(interval)
+    }
+})
+let disp: Subscription;
+
 class RxjPlay extends React.Component<{}, State>{
-    source: Observable<Event> | null = null
+    source$: Observable<Event> | null = null
     content: Date[] = []
     myRef = React.createRef<HTMLButtonElement>()
     constructor(props: any) {
@@ -21,15 +41,15 @@ class RxjPlay extends React.Component<{}, State>{
         this.state = { total: 0 }
     }
     componentDidMount() {
-        if (this.myRef.current) {
-            this.source = fromEvent(this.myRef.current, 'click')
-            const example = this.source.pipe(
-                bufferCount(2),
-                map(event => "double clicked")
-            )
-            example.subscribe(val => this.addItem(val))
-        }
-
+        // if (this.myRef.current) {
+        //     this.source$ = fromEvent(this.myRef.current, 'click')
+        //     const foo = interval(200).pipe
+        //     const example = this.source$.pipe(
+        //         bufferCount(5),
+        //         map(event => "double clicked")
+        //     )
+        //     example.subscribe(val => this.addItem(val))
+        // }
 
     }
 
@@ -37,13 +57,38 @@ class RxjPlay extends React.Component<{}, State>{
         const total = this.state.total + 1
         this.setState({ total })
     }
+
+    onClick = () => {
+
+        const subscriber: Observer<number> = {
+            next: (x) => {
+                console.log(x)
+            },
+            error: (err) => {
+                console.log(err)
+            },
+            complete: () => {
+                console.log('complete')
+            }
+        }
+        const b = x.pipe(map(x => x + x))
+        disp = b.subscribe(subscriber)
+    }
+
+    onClick1 = () => {
+        disp.unsubscribe()
+    }
+
     render() {
         return (
             <Container >
-                <button ref={this.myRef} id="danger">Double Click to see changes</button>
+                <button onClick={this.onClick} >Subscribe</button>
+                <button onClick={this.onClick1} >Unsubscribe</button>
+
+                {/* <button ref={this.myRef} >Double Click to see changes</button>
                 <div>
                     Total Number you have double clicked: {this.state.total}
-                </div>
+                </div> */}
             </Container>
         )
     }
